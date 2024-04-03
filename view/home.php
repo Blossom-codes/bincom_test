ec020a09412fbd6c100fbb067266d5cbba56e1f1
Blossom-codes <blozzyblack@gmail.com>
2024-04-03 14:15:31 +0100

    home

--- view/home.php
+++ view/home.php
@@ -10,7 +10,7 @@
                 <a class="text-decoration-none link-dark" href="view_total_results.php">
                     <div class="card bg-warning text-light p-3 p-md-4">
                         <div class="card-body">
-                            <h5 class="card-title text-center">Total LGA Result</h5>
+                            <h5 class="card-title text-center">Store New Results</h5>
                             <p class="text-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-save">
                                     <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path>
