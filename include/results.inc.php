49b2d0b49265e9c25e2b5ec4d4f754bdf6dfbc07
Blossom-codes <blozzyblack@gmail.com>
2024-04-03 20:27:29 +0100

    commit

--- include/results.inc.php
+++ include/results.inc.php
@@ -22,28 +22,31 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['reset'])) {
     unset($_SESSION['ward']);
     header("location: ../view/view_pu_results.php");
 }
-// if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit_lga'])) {
-//     $user = new User_contr;
-//     $lga_id = $_POST['lga'];
-//     $ward = $user->viewData("ward", "lga_id", $lga_id);
-//     $totalScore =  0;
-//     while ($wardRow = $ward->fetch()) {
-//         echo "<pre>".  $ward_id = $wardRow['ward_id'];
-//         $totalScore += $ward_id;
-//         $pollingUnit = $user->viewDataWithTwoParam("polling_unit", "ward_id", "$ward_id", "lga_id", "$lga_id");
-//             $pollingUnitRow = $pollingUnit->fetch();
-//                 $pollingUnitUniqueId = $pollingUnitRow['uniqueid'];
-//                 echo "<pre>". $pollingUnitUniqueId ?? "";
-//         //         $pollingUnitScore = $user->viewData("announced_pu_results", "polling_unit_uniqueid", $pollingUnitUniqueId);
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit_lga'])) {
+    $user = new User_contr;
+    echo "<pre> lga_id " . $lga_id = $_POST['lga'];
+    
+    $lga = $user->viewData("lga", "lga_id", $lga_id);
+    $lgaRow = $lga->fetch();
+    $lga_name = $lgaRow['lga_name'];
+    $ward = $user->viewData("ward", "lga_id", $lga_id);
+    $totalScore =  0;
+    while ($wardRow = $ward->fetch()) {
+        echo  "<pre> ward " . $ward_id = $wardRow['ward_id'];
+        // echo  "<pre> total ".$totalScore += $ward_id;
 
-//         //         $pollingUnitScoreRow = $pollingUnitScore->fetch();
-//         //          $pollingUnitScoreRow['party_score'] ?? "";
-//         //          $totalScore += $pollingUnitScoreRow['party_score'];                 
-//         //         }
+        $pollingUnit = $user->viewDataWithTwoParam("polling_unit", "ward_id", "$ward_id", "lga_id", "$lga_id");
+        $pollingUnitRow = $pollingUnit->fetch();
 
-//     }
-//     echo "<pre>" . $totalScore;
-//     // echo $totalScore ?? "";
-// header("location: ../view/view_total_results.php");
+        $pollingUnitUniqueId = $pollingUnitRow['uniqueid'] ?? 0;
+        echo "<pre> unique_id " . $pollingUnitUniqueId;
+        if ($pollingUnitUniqueId != 0) {
+            $pollingUnitScore = $user->viewData("announced_pu_results", "polling_unit_uniqueid", $pollingUnitUniqueId);
+            $pollingUnitScoreRow = $pollingUnitScore->fetch();
+            $partyScore = $pollingUnitScoreRow['party_score'] ?? 0;
+            $totalScore += $partyScore;
+        }
+    }
 
-// }
+    header("location: ../view/view_total_results.php?lga_name=$lga_name&&total_score=$totalScore");
+}
--- view/view_total_results.php
+++ view/view_total_results.php
@@ -9,26 +9,41 @@
                 <div class="card-body">
                     <h5 class="card-title">Please select a local government area(LGA)</h5>
                     <hr>
-                   
-                    <form action="../include/results.inc.php" method="post">
-                        <div class="form-floating">
-                            <select class="form-select" name="lga" id="floatingSelect" aria-label="Floating label select example">
-                                <?php
-                                $user = new User_contr;
-                                $stmt = $user->viewData("lga", "state_id", 25);
-                                while ($row = $stmt->fetch()) {
 
-                                ?>
-                                    <option value="<?= $row['lga_id'] ?>"><?= $row['lga_name'] ?></option>
-     
-                                <?php
-                                }
-                                ?>
-                            </select>
-                            <label for="floatingSelect">LGA</label>
+                    <?php
+                    if (!isset($_GET['total_score'])) {
+                    ?>
+                        <form action="../include/results.inc.php" method="post">
+                            <div class="form-floating">
+                                <select class="form-select" name="lga" id="floatingSelect" aria-label="Floating label select example">
+                                    <?php
+                                    $user = new User_contr;
+                                    $stmt = $user->viewData("lga", "state_id", 25);
+                                    while ($row = $stmt->fetch()) {
+
+                                    ?>
+                                        <option value="<?= $row['lga_id'] ?>"><?= $row['lga_name'] ?></option>
+
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                                <label for="floatingSelect">LGA</label>
+                            </div>
+                            <button type="submit" name="submit_lga" class="btn btn-primary w-100 mt-2">Submit</button>
+                        </form>
+                        <?php
+                    } else {
+                        ?>
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Summed Total Results for <?=$_GET['lga_name']?> </h5>
+                                <p class="card-text fw-light fs-1"><?= $_GET['total_score']?></p>
+                            </div>
                         </div>
-                        <button type="submit" name="submit_lga" class="btn btn-primary w-100 mt-2">Submit</button>
-                    </form>
+                        <?php
+                        }
+                            ?>
                 </div>
             </div>
         </div>
